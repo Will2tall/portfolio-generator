1f49032099c4c716f11bf0961d958e2943d9871f
Terror666 <ckuthulu@gmail.com>
2021-08-11 22:46:07 -0500

    inquirer download

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-/*const name = profileDataArgs[0];
-const github = profileDataArgs[1];*/
-const [name, github] = profileDataArgs;
+inquirer
+.prompt([
+    {type: 'input',
+    name: 'name',
+    message: 'What is your name?'}
+])
+.then(answers => console.log(answers));
+/*const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
     
     console.log('Portfolio complete! Check out index.html to see the output!')
-});
-/*const printProfileData = (profileDataArr) => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    } 
-    
-    console.log('================');
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs)*/
+});*/
