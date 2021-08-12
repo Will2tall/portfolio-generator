b0fd22d3e65c41c736a2a04ee8065c71b069a09c
Terror666 <ckuthulu@gmail.com>
2021-08-11 23:31:07 -0500

    resume questionare

--- app.js
+++ app.js
@@ -22,10 +22,22 @@ return inquirer.prompt([
         }else {
             console.log('Please enter your GitHub username!');
             return false;
+        }
     }},
-    type: 'input',
+    {type: 'confirm',
+    name: 'confirmAbout',
+    message: 'Would you like to enter some information about yourself for an "About" section?',
+    default: true},
+    {type: 'input',
     name:'about',
-    message: 'Provide some information about yourself:'}
+    message: 'Provide some information about yourself:',
+    when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+            return true;
+        }else {
+            return false;
+        }
+    }}
     ]);
 };
 
