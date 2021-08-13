dc83147cec47e8a76c404cfb48cb32a676a09098
Terror666 <ckuthulu@gmail.com>
2021-08-13 01:04:36 -0500

    html forming

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -123,12 +126,17 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-    });
-/*const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+        const pageHTML = generatePage(portfolioData);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
+        fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Page created! Check out index.html in this directory to see it!');
+        });
+    });
+
+
+/*fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
     
     console.log('Portfolio complete! Check out index.html to see the output!')
